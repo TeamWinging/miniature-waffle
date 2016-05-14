811dbe88724d7fbacfd3e6ee77facebf866d9aed
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2016-05-14 11:12:41 +0200

    Removed getscreen endpoint
    Converted it to an internal function

--- app/views.py
+++ app/views.py
@@ -13,8 +13,13 @@ def index():
     return 'Rachele stinks!'
 
 
-@app.route('/screen/<id>', methods=('GET',))
-def getscreen(id):
+def getscreen(app_id):
+    '''
+    Gets the screen for the app_id
+
+    if there is no screen, returns a blank one
+    '''
+    return data.get(app_id, [ [i for i in bytes(8)] for i in range(8)])
     return repr(data[id])
 
 
@@ -69,10 +74,10 @@ def setpixel(app_id, x, y):
     '''
     x = int(x)
     y = int(y)
-    screen = data.get(app_id, [ [i for i in bytes(8)] for i in range(8)])
     value = int(request.stream.read())
     if value not in range(2):
         raise Exception('Pixel value out of range')
+    screen = getscreen(app_id)
     screen[x][y] = value
     data[app_id] = screen
     return repr(screen)
